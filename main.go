8f07b38492792079239bd2a13ea7e421b1e834fd
rajpurohitnikki121-hue <rajpurohitnikki121@gmail.com>
2026-09-25 14:47:52 +0530

    Update LDFLAGS for Linux in main.go
    Added -lresolv to LDFLAGS for Linux build.

--- main.go
+++ main.go
@@ -9,7 +9,7 @@
 package main
 
 /*
-#cgo linux LDFLAGS: -L . -lntgcalls -lm -lz
+#cgo linux LDFLAGS: -L . -lntgcalls -lm -lz -lresolv
 #cgo darwin LDFLAGS: -L . -lntgcalls -lc++ -lz -lbz2 -liconv -framework AVFoundation -framework AudioToolbox -framework CoreAudio -framework QuartzCore -framework CoreMedia -framework VideoToolbox -framework AppKit -framework Metal -framework MetalKit -framework OpenGL -framework IOSurface -framework ScreenCaptureKit
 
 // Currently is supported only dynamically linked library on Windows due to
@@ -46,6 +46,7 @@ func main() {
 
 	tdDir := "database"
 	_ = os.Remove(tdDir)
+
 	libPath := "./libtdjson.so.1.8.67"
 	manager := gotdbot.NewClientManager(libPath)
 
@@ -83,6 +84,7 @@ func main() {
 			config.DlBotToken,
 			dlClientConfig,
 		)
+
 		if err != nil {
 			client.Logger.Warnf(
 				"failed to register dl client: %s",
